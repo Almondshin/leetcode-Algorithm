da3ce016f14a80c8535770aa5974f9a8cdc37a5a
MidnightStudio <88614978+Almondshin@users.noreply.github.com>
2022-04-05 17:29:32 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (37.41%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <=2){
+            return n;
+        }
+
+        int [] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 2;
+
+        for (int i = 2; i < n ; i++) {
+            answer[i] = answer[i-2] + answer[i-1];
+        }
+
+        return answer[n-1];
+        
+    }
+}
